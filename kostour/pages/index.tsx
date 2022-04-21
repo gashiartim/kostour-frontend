c741073f485d0a1c4cb18b4d10f2b197e3d4111f
Naime <naime@Naimes-MacBook-Pro.local>
2022-04-21 23:38:37 +0200

    Add project setup

---
+++ kostour/pages/index.tsx
@@ -0,0 +1,13 @@
+import styles from '../styles/Home.module.css'
+
+const Home = () => {
+  return (
+    <div className={styles.container}>
+        <h3 className={styles.title}>
+          Hello KosTour
+        </h3>
+    </div>
+  )
+}
+
+export default Home
